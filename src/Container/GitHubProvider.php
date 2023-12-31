9b34c68638007329c7396e0df5b32647e972a2e3
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-12-31 18:39:50 +0200

    Update copyright for 2024

--- src/Container/GitHubProvider.php
+++ src/Container/GitHubProvider.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * @package   panopticon
+ * @copyright Copyright (c)2023-2024 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   https://www.gnu.org/licenses/agpl-3.0.txt GNU Affero General Public License, version 3 or later
+ */
 
 namespace Akeeba\CoreSums\Container;
 
